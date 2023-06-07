7f08d82eda66fb7ec3abf4ff77400e498362067f
kpisti18 <64091644+kpisti18@users.noreply.github.com>
2023-06-07 22:07:20 +0200

    Add files via upload

---
+++ Konyv.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kovacs_Istvan_12_console
+{
+    internal class Konyv
+    {
+        public int KonyvID { get; set; }
+        public string Szerzo { get; set; }
+        public string Cim { get; set; }
+        public string ISBN { get; set; }
+        public int Ar { get; set; }
+
+        public Konyv(int konyvID, string szerzo, string cim, string iSBN, int ar)
+        {
+            KonyvID = konyvID;
+            Szerzo = szerzo;
+            Cim = cim;
+            ISBN = iSBN;
+            Ar = ar;
+        }
+
+        public override string ToString()
+        {
+            return $"{Szerzo}: {Cim}";
+        }
+    }
+}
